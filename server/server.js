56aae9d84b733b0c7fc124074ab96c54712a63db
Prashant Kumar Singh <prashantsingh5356@gmail.com>
2023-07-06 20:49:31 +0530

    conflict resolved

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
@@ -17,19 +16,3 @@ mongoose.connect(DB).then(() => {
 });
 
 app.listen(port, console.log(`server started at port ${port}`));
-=======
-require("dotenv").config();
-const app = require("./app");
-const mongoose = require("mongoose");
-
-let port = process.env.PORT || 3000;
-
-app.listen(port, async () => {
-  await mongoose
-    .connect(process.env.MONGO_URL)
-    .then((data) => console.log("Connected to DB"))
-    .catch((err) => console.log("Error connecting to DB", err));
-
-  console.log(`Server listening on port: ${port}`);
-});
->>>>>>> 1ef577d18cf086f782d9f336fa7c5faf4c609232
