a154f1ab81ab9411e2c5ce0adb930046dae522a5
Prashant Kumar Singh <prashantsingh5356@gmail.com>
2023-07-08 10:27:23 +0530

    added document model and controllers

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import userRoute from "./routes/usersRoute.js";
+import documentsRoute from "./routes/documentsRoute.js";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/users", userRoute);
+app.use("/api/users", documentsRoute);
 
 export default app;
---
+++ server/models/documentModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema({
+  authorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "An document must have an author "],
+  },
+  type: {
+    type: String,
+    required: [true, "please mention type of document"],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now().toString(),
+  },
+
+  lastModified: {
+    type: Date,
+    default: Date.now().toString(),
+  },
+
+  title: {
+    type: String,
+    required: [true, "An document must have an title"],
+  },
+
+  tags: {
+    type: [String],
+    required: [true, "An document must have an tag"],
+  },
+
+  heading: {
+    type: String,
+    required: [true, "An document must have an heading"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "An document must have an description"],
+  },
+});
+
+const Document = mongoose.model("Document", documentSchema);
+
+export default Document;
