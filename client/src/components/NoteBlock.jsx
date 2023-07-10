3e63bae77fff993f0ff1a8b3c6c1375145fb8ac0
Prashant Kumar Singh <prashantsingh5356@gmail.com>
2023-07-10 21:05:05 +0530

    added update and delete functionality

--- client/src/components/NoteBlock.jsx
+++ client/src/components/NoteBlock.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import BASE_URL from "../serverInfo";
+import { AiFillDelete } from "react-icons/ai";
+import { BiSolidEditAlt } from "react-icons/bi";
 
 let months = {
   1: "JAN",
@@ -12,30 +15,68 @@ let months = {
   9: "SEP",
   10: "OCT",
   11: "NOV",
-  12: "DEC"
-}
+  12: "DEC",
+};
 
-function NoteBlock({ isSelected, onClick, title, description, date }) {
+function NoteBlock({
+  isSelected,
+  onClick,
+  title,
+  description,
+  date,
+  noteId,
+  removeNote,
+  updateNote,
+  setUpdateHandler,
+  updateIdHandler,
+}) {
   const day = new Date(date).getDate();
   const month = new Date(date).getMonth() + 1;
-  const year = new Date(date).getFullYear();
 
-  // console.log(date);
+  const deleteNoteHandler = async (id) => {
+    console.log(id);
+    await fetch(`${BASE_URL}/api/users/note/${id}`, {
+      method: "DELETE", // HTTP method
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        removeNote(id);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const editNoteHandler = (id) => {
+    updateNote(true);
+    setUpdateHandler(true);
+    updateIdHandler(id);
+  };
 
   return (
     <div
-      className={`w-[100%] h-[10vh] px-2 py-2 mb-4 rounded-lg ${
+      className={`w-[100%] min-h-[15vh] px-2 py-2 mb-4 rounded-lg ${
         isSelected === title ? "bg-gray-200" : "bg-slate-200/50"
       } cursor-pointer`}
       onClick={onClick}
     >
-      <p
-        className={`font-bold text-xs ${
-          isSelected === title ? "text-black" : "text-gray-400"
-        }`}
-      >
-        {`${day} ${months[month]}`}
-      </p>
+      <div className="flex justify-between">
+        <p
+          className={`font-bold text-xs ${
+            isSelected === title ? "text-black" : "text-gray-400"
+          }`}
+        >
+          {`${day} ${months[month]}`}
+        </p>
+        <div className="flex ">
+          <BiSolidEditAlt
+            className="mr-3"
+            onClick={() => editNoteHandler(noteId)}
+          />
+          <AiFillDelete onClick={() => deleteNoteHandler(noteId)} />
+        </div>
+      </div>
       <h2
         className={`font-bold text-lg  my-2 ${
           isSelected === title ? "text-black" : "text-gray-400"
@@ -48,7 +89,9 @@ function NoteBlock({ isSelected, onClick, title, description, date }) {
           isSelected === title ? "text-black" : "text-gray-400"
         } break-words`}
       >
-        {description.length > 80 ? description.slice(0, 80) + "..." : description}
+        {description.length > 80
+          ? description.slice(0, 80) + "..."
+          : description}
       </p>
     </div>
   );
